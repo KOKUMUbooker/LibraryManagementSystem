45bc35f2d7f14a1c8cbb976ea1a1088735c8f14b
KOKUMUbooker <bookerochieng20@gmail.com>
2026-02-16 14:41:34 +0300

    Implement all Book methods for Library Service

--- LibraryManagementSystem.App/Services/LibraryService.cs
+++ LibraryManagementSystem.App/Services/LibraryService.cs
@@ -9,10 +9,35 @@ public class LibraryService : ILibraryService
     private readonly List<Loan> _loans = new();
 
     // Book Methods
-    public Book AddBook(string title, string author, int copies);
-    public bool RemoveBook(Guid bookId);
-    public List<Book> GetAllBooks();
-    public List<Book> SearchBooksByTitle(string title);
+    public Book AddBook(string title, string author, int copies)
+    {
+        if (copies <= 0) throw new ArgumentException("Copies must be greater than 0");
+
+        var book = new Book(title, author, copies);
+        _books.Add(book);
+        return book;
+    }
+
+    public bool RemoveBook(Guid bookId)
+    {
+        var book = _books.FirstOrDefault(b => b.Id == bookId);
+        if (book == null) return false;
+
+        if (_loans.Any(l => l.BookId == bookId)) return false; // If has active loans
+
+        _books.Remove(book);
+        return true;
+    }
+
+    public List<Book> GetAllBooks()
+    {
+        return _books.ToList();
+    }
+
+    public List<Book> SearchBooksByTitle(string title)
+    {
+        return _books.Where(b => b.Title.Contains(title, StringComparison.OrdinalIgnoreCase)).ToList();
+    };
 
     // Member Methods
     public Member RegisterMember(string name);
